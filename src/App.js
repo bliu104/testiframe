967cd9208ae45d55b7413965db82e1cb215c0391
bliu104 <bliu104@gmail.com>
2021-08-16 16:52:39 -0400

    added iframes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+ import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <iframe src="http://localhost:3001/1" height="850px" width="1250px" title="TAPTAB web menu"></iframe>
+
+    </div>
+  );
+}
+
+export default App;
